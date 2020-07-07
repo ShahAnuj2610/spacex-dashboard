3b88508174dfa36a1dc2a54bf6c61ad22ba71618
anuj <anujshah584@gmail.com>
2020-07-07 15:02:45 +0530

    fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
     theme: {
         extend: {
             height: {
                 "100": "25rem",
             },
-        },
-        colors: {
-            ...defaultTheme.colors,
-            primary: "#ffffff",
-            dark: "#0e0e10",
-            darkFont: "rgb(239, 239, 241)",
         },
     },
     variants: {},
